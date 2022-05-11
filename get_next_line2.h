26693a6bed6d70f74c5fe406e3762c2d4ffcfe3f
Ian Antony Mongeon Charest <i-antony@hotmail.com>
2022-05-10 22:29:29 -0400

    gnl : oops norminette proof header file too :p

--- get_next_line2.h
+++ get_next_line2.h
@@ -6,11 +6,11 @@
 /*   By: iamongeo <marvin@42quebec.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/04 15:10:58 by iamongeo          #+#    #+#             */
-/*   Updated: 2022/05/10 22:04:21 by iamongeo         ###   ########.fr       */
+/*   Updated: 2022/05/10 22:28:18 by iamongeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef	GET_NEXT_LINE2_H
+#ifndef GET_NEXT_LINE2_H
 # define GET_NEXT_LINE2_H
 
 # ifndef BUFFER_SIZE
@@ -31,7 +31,7 @@ typedef struct s_dlst
 int	malloc_ptr(size_t n, void **ret_ptr);
 int	ft_substr(const char *str, size_t start, size_t n, char **ret);
 int	dlst_insert(t_dlst **dlst, t_dlst **elem, char *mstr, int push_app);
-int join_clear_list(char *line, t_dlst **elem, int do_join);
+int	join_clear_list(char *line, t_dlst **elem, int do_join);
 int	gather_line(t_dlst **chks, char **ret_line, size_t *n_chrs);
 
 enum	e_fail_codes
